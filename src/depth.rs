99b7074562de87b7caa6bce1efc246b33f2820fc
Fu Wang <wangfu91@hotmail.com>
2025-06-11 19:41:00 +0800

    Update depth.rs

--- src/depth.rs
+++ src/depth.rs
@@ -1,20 +1,349 @@
-use crate::bindings::{IDepthFrameReference, IDepthFrameSource};
+use crate::bindings::{
+    BOOLEAN, IDepthFrame, IDepthFrameArrivedEventArgs, IDepthFrameReader, IDepthFrameReference,
+    IDepthFrameSource, IFrameCapturedEventArgs, IFrameDescription, IKinectSensor, TIMESPAN, UINT,
+    UINT16, WAITABLE_HANDLE,
+};
+use crate::frame::FrameCapturedEventArgs;
 use std::ptr;
+use windows::{
+    Win32::Foundation::{E_FAIL, E_POINTER},
+    core::Error,
+};
 
 pub struct DepthFrameSource {
     ptr: *mut IDepthFrameSource,
 }
+
 impl DepthFrameSource {
     pub(crate) fn new(ptr: *mut IDepthFrameSource) -> Self {
         assert!(!ptr.is_null());
         Self { ptr }
     }
+
+    pub fn subscribe_frame_captured(&self) -> Result<WAITABLE_HANDLE, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let subscribe_fn = vtbl.SubscribeFrameCaptured.ok_or(E_FAIL)?;
+        let mut waitable_handle: WAITABLE_HANDLE = windows::Win32::Foundation::HANDLE::default();
+        let hr = unsafe { subscribe_fn(self.ptr, &mut waitable_handle) };
+        if hr.is_ok() {
+            Ok(waitable_handle)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn unsubscribe_frame_captured(
+        &self,
+        waitable_handle: WAITABLE_HANDLE,
+    ) -> Result<(), Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let unsubscribe_fn = vtbl.UnsubscribeFrameCaptured.ok_or(E_FAIL)?;
+        let hr = unsafe { unsubscribe_fn(self.ptr, waitable_handle) };
+        if hr.is_ok() {
+            Ok(())
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_frame_captured_event_data(
+        &self,
+        waitable_handle: WAITABLE_HANDLE,
+    ) -> Result<FrameCapturedEventArgs, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_data_fn = vtbl.GetFrameCapturedEventData.ok_or(E_FAIL)?;
+        let mut event_data: *mut IFrameCapturedEventArgs = ptr::null_mut();
+        let hr = unsafe { get_data_fn(self.ptr, waitable_handle, &mut event_data) };
+        if hr.is_ok() {
+            Ok(FrameCapturedEventArgs::new(event_data))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_is_active(&self) -> Result<bool, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_active_fn = vtbl.get_IsActive.ok_or(E_FAIL)?;
+        let mut is_active: BOOLEAN = 0;
+        let hr = unsafe { get_active_fn(self.ptr, &mut is_active) };
+        if hr.is_ok() {
+            Ok(is_active != 0)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn open_reader(&self) -> Result<DepthFrameReader, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let open_fn = vtbl.OpenReader.ok_or(E_FAIL)?;
+        let mut reader_ptr: *mut IDepthFrameReader = ptr::null_mut();
+        let hr = unsafe { open_fn(self.ptr, &mut reader_ptr) };
+        if hr.is_ok() {
+            Ok(DepthFrameReader::new(reader_ptr))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_depth_min_reliable_distance(&self) -> Result<UINT16, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_DepthMinReliableDistance.ok_or(E_FAIL)?;
+        let mut distance: UINT16 = 0;
+        let hr = unsafe { get_fn(self.ptr, &mut distance) };
+        if hr.is_ok() {
+            Ok(distance)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_depth_max_reliable_distance(&self) -> Result<UINT16, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_DepthMaxReliableDistance.ok_or(E_FAIL)?;
+        let mut distance: UINT16 = 0;
+        let hr = unsafe { get_fn(self.ptr, &mut distance) };
+        if hr.is_ok() {
+            Ok(distance)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_frame_description(&self) -> Result<*mut IFrameDescription, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_FrameDescription.ok_or(E_FAIL)?;
+        let mut frame_description: *mut IFrameDescription = ptr::null_mut();
+        let hr = unsafe { get_fn(self.ptr, &mut frame_description) };
+        if hr.is_ok() {
+            Ok(frame_description)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_kinect_sensor(&self) -> Result<*mut IKinectSensor, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_KinectSensor.ok_or(E_FAIL)?;
+        let mut sensor_ptr: *mut IKinectSensor = ptr::null_mut();
+        let hr = unsafe { get_fn(self.ptr, &mut sensor_ptr) };
+        if hr.is_ok() {
+            Ok(sensor_ptr)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
 }
+
 impl Drop for DepthFrameSource {
     fn drop(&mut self) {
         if !self.ptr.is_null() {
             unsafe {
-                ((*(*self.ptr).lpVtbl).Release.unwrap())(self.ptr);
+                if let Some(vtbl) = (*self.ptr).lpVtbl.as_ref() {
+                    if let Some(release_fn) = vtbl.Release {
+                        release_fn(self.ptr);
+                    }
+                }
+            }
+            self.ptr = ptr::null_mut();
+        }
+    }
+}
+
+pub struct DepthFrameReader {
+    ptr: *mut IDepthFrameReader,
+}
+
+impl DepthFrameReader {
+    pub(crate) fn new(ptr: *mut IDepthFrameReader) -> Self {
+        assert!(!ptr.is_null());
+        Self { ptr }
+    }
+
+    pub fn subscribe_frame_arrived(&self) -> Result<WAITABLE_HANDLE, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let subscribe_fn = vtbl.SubscribeFrameArrived.ok_or(E_FAIL)?;
+        let mut waitable_handle: WAITABLE_HANDLE = windows::Win32::Foundation::HANDLE::default();
+        let hr = unsafe { subscribe_fn(self.ptr, &mut waitable_handle) };
+        if hr.is_ok() {
+            Ok(waitable_handle)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn unsubscribe_frame_arrived(&self, waitable_handle: WAITABLE_HANDLE) -> Result<(), Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let unsubscribe_fn = vtbl.UnsubscribeFrameArrived.ok_or(E_FAIL)?;
+        let hr = unsafe { unsubscribe_fn(self.ptr, waitable_handle) };
+        if hr.is_ok() {
+            Ok(())
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_frame_arrived_event_data(
+        &self,
+        waitable_handle: WAITABLE_HANDLE,
+    ) -> Result<DepthFrameArrivedEventArgs, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.GetFrameArrivedEventData.ok_or(E_FAIL)?;
+        let mut args_ptr: *mut IDepthFrameArrivedEventArgs = ptr::null_mut();
+        let hr = unsafe { get_fn(self.ptr, waitable_handle, &mut args_ptr) };
+        if hr.is_ok() {
+            Ok(DepthFrameArrivedEventArgs::new(args_ptr))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn acquire_latest_frame(&self) -> Result<DepthFrame, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let acquire_fn = vtbl.AcquireLatestFrame.ok_or(E_FAIL)?;
+        let mut frame_ptr: *mut IDepthFrame = ptr::null_mut();
+        let hr = unsafe { acquire_fn(self.ptr, &mut frame_ptr) };
+        if hr.is_ok() {
+            Ok(DepthFrame::new(frame_ptr))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_is_paused(&self) -> Result<bool, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_IsPaused.ok_or(E_FAIL)?;
+        let mut is_paused: BOOLEAN = 0;
+        let hr = unsafe { get_fn(self.ptr, &mut is_paused) };
+        if hr.is_ok() {
+            Ok(is_paused != 0)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn set_is_paused(&self, is_paused: bool) -> Result<(), Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let put_fn = vtbl.put_IsPaused.ok_or(E_FAIL)?;
+        let paused_value: BOOLEAN = if is_paused { 1 } else { 0 };
+        let hr = unsafe { put_fn(self.ptr, paused_value) };
+        if hr.is_ok() {
+            Ok(())
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_depth_frame_source(&self) -> Result<DepthFrameSource, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_DepthFrameSource.ok_or(E_FAIL)?;
+        let mut source_ptr: *mut IDepthFrameSource = ptr::null_mut();
+        let hr = unsafe { get_fn(self.ptr, &mut source_ptr) };
+        if hr.is_ok() {
+            Ok(DepthFrameSource::new(source_ptr))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+}
+
+impl Drop for DepthFrameReader {
+    fn drop(&mut self) {
+        if !self.ptr.is_null() {
+            unsafe {
+                if let Some(vtbl) = (*self.ptr).lpVtbl.as_ref() {
+                    if let Some(release_fn) = vtbl.Release {
+                        release_fn(self.ptr);
+                    }
+                }
+            }
+            self.ptr = ptr::null_mut();
+        }
+    }
+}
+
+pub struct DepthFrameArrivedEventArgs {
+    ptr: *mut IDepthFrameArrivedEventArgs,
+}
+
+impl DepthFrameArrivedEventArgs {
+    pub(crate) fn new(ptr: *mut IDepthFrameArrivedEventArgs) -> Self {
+        assert!(!ptr.is_null());
+        Self { ptr }
+    }
+
+    pub fn get_frame_reference(&self) -> Result<DepthFrameReference, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_FrameReference.ok_or(E_FAIL)?;
+        let mut ref_ptr: *mut IDepthFrameReference = ptr::null_mut();
+        let hr = unsafe { get_fn(self.ptr, &mut ref_ptr) };
+        if hr.is_ok() {
+            Ok(DepthFrameReference::new(ref_ptr))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+}
+
+impl Drop for DepthFrameArrivedEventArgs {
+    fn drop(&mut self) {
+        if !self.ptr.is_null() {
+            unsafe {
+                if let Some(vtbl) = (*self.ptr).lpVtbl.as_ref() {
+                    if let Some(release_fn) = vtbl.Release {
+                        release_fn(self.ptr);
+                    }
+                }
             }
             self.ptr = ptr::null_mut();
         }
@@ -30,9 +359,174 @@ impl DepthFrameReference {
         assert!(!ptr.is_null());
         Self { ptr }
     }
+
+    pub fn acquire_frame(&self) -> Result<DepthFrame, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let acquire_fn = vtbl.AcquireFrame.ok_or(E_FAIL)?;
+        let mut frame_ptr: *mut IDepthFrame = ptr::null_mut();
+        let hr = unsafe { acquire_fn(self.ptr, &mut frame_ptr) };
+        if hr.is_ok() {
+            Ok(DepthFrame::new(frame_ptr))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_relative_time(&self) -> Result<TIMESPAN, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_RelativeTime.ok_or(E_FAIL)?;
+        let mut time: TIMESPAN = 0;
+        let hr = unsafe { get_fn(self.ptr, &mut time) };
+        if hr.is_ok() {
+            Ok(time)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
 }
 
 impl Drop for DepthFrameReference {
+    fn drop(&mut self) {
+        if !self.ptr.is_null() {
+            unsafe {
+                if let Some(vtbl) = (*self.ptr).lpVtbl.as_ref() {
+                    if let Some(release_fn) = vtbl.Release {
+                        release_fn(self.ptr);
+                    }
+                }
+            }
+            self.ptr = ptr::null_mut();
+        }
+    }
+}
+
+pub struct DepthFrame {
+    ptr: *mut IDepthFrame,
+}
+
+impl DepthFrame {
+    pub(crate) fn new(ptr: *mut IDepthFrame) -> Self {
+        assert!(!ptr.is_null());
+        Self { ptr }
+    }
+
+    pub fn copy_frame_data_to_array(
+        &self,
+        capacity: UINT,
+        frame_data: *mut UINT16,
+    ) -> Result<(), Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let copy_fn = vtbl.CopyFrameDataToArray.ok_or(E_FAIL)?;
+        let hr = unsafe { copy_fn(self.ptr, capacity, frame_data) };
+        if hr.is_ok() {
+            Ok(())
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn access_underlying_buffer(&self) -> Result<(UINT, *mut UINT16), Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let access_fn = vtbl.AccessUnderlyingBuffer.ok_or(E_FAIL)?;
+        let mut capacity: UINT = 0;
+        let mut buffer: *mut UINT16 = ptr::null_mut();
+        let hr = unsafe { access_fn(self.ptr, &mut capacity, &mut buffer) };
+        if hr.is_ok() {
+            Ok((capacity, buffer))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_frame_description(&self) -> Result<*mut IFrameDescription, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_FrameDescription.ok_or(E_FAIL)?;
+        let mut frame_description: *mut IFrameDescription = ptr::null_mut();
+        let hr = unsafe { get_fn(self.ptr, &mut frame_description) };
+        if hr.is_ok() {
+            Ok(frame_description)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_relative_time(&self) -> Result<TIMESPAN, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_RelativeTime.ok_or(E_FAIL)?;
+        let mut time: TIMESPAN = 0;
+        let hr = unsafe { get_fn(self.ptr, &mut time) };
+        if hr.is_ok() {
+            Ok(time)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_depth_frame_source(&self) -> Result<DepthFrameSource, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_DepthFrameSource.ok_or(E_FAIL)?;
+        let mut source_ptr: *mut IDepthFrameSource = ptr::null_mut();
+        let hr = unsafe { get_fn(self.ptr, &mut source_ptr) };
+        if hr.is_ok() {
+            Ok(DepthFrameSource::new(source_ptr))
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_depth_min_reliable_distance(&self) -> Result<UINT16, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_DepthMinReliableDistance.ok_or(E_FAIL)?;
+        let mut distance: UINT16 = 0;
+        let hr = unsafe { get_fn(self.ptr, &mut distance) };
+        if hr.is_ok() {
+            Ok(distance)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+
+    pub fn get_depth_max_reliable_distance(&self) -> Result<UINT16, Error> {
+        if self.ptr.is_null() {
+            return Err(Error::from_hresult(E_POINTER));
+        }
+        let vtbl = unsafe { (*self.ptr).lpVtbl.as_ref() }.ok_or(E_POINTER)?;
+        let get_fn = vtbl.get_DepthMaxReliableDistance.ok_or(E_FAIL)?;
+        let mut distance: UINT16 = 0;
+        let hr = unsafe { get_fn(self.ptr, &mut distance) };
+        if hr.is_ok() {
+            Ok(distance)
+        } else {
+            Err(Error::from_hresult(hr))
+        }
+    }
+}
+
+impl Drop for DepthFrame {
     fn drop(&mut self) {
         if !self.ptr.is_null() {
             unsafe {
