a125ac516e5c86e3b75bbc51a9e18d1f01fc4ffa
Fu Wang <wangfu91@hotmail.com>
2025-06-12 13:48:13 +0800

    Update.

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub mod audio;
+pub mod bindings;
+pub mod body;
+pub mod body_index;
+pub mod color;
+pub mod coordinate;
+pub mod depth;
+pub mod frame;
+pub mod infrared;
+pub mod kinect;
+pub mod long_exposure_infrared;
+pub mod multi_source_frame;
+pub mod utils;
+
+pub const FRAME_WAIT_TIMEOUT_MS: u32 = 100;
